9106b6249094ec9b78f6ee1970f4d0be0ab87c7a
LeagueofDouzy <60365168+LeagueofDouzy@users.noreply.github.com>
2020-12-18 17:19:18 +0000

    ProjectUpload

---
+++ IMMProjectBetaFiles/Assets/Scripts/ProjectileBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileBehaviour : MonoBehaviour
+{
+    private float projectileSpeed = 75;
+    private float outOfBounds = 45;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.up * Time.deltaTime * projectileSpeed);         //ProjectileMovement
+
+        if (transform.position.x > outOfBounds)
+        {
+            Destroy(gameObject);                                                     //Destroy when out of bounds
+        }
+    }
+}
